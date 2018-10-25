f2f20ee8c3e441dff762d92d38e75b5176f1657c
jimczj <jimczj@gmail.com>
2018-10-25 23:35:48 +0800

    feat: 增加端口配置

--- start.sample.sh
+++ start.sample.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 export HUBOT_BEARYCHAT_TOKENS=token-token-token-here
-export HUBOT_BEARYCHAT_MODE=rtm
+export HUBOT_BEARYCHAT_MODE=http
+export EXPRESS_PORT=7001
 ./bin/hubot -a bearychat
