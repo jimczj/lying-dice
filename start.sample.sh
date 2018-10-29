61002f4d3adfb2d19ea7118e903a02db38f75574
jimczj <jimczj@gmail.com>
2018-10-30 01:56:03 +0800

    feat: 换成 rtm 模式

--- start.sample.sh
+++ start.sample.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export HUBOT_BEARYCHAT_TOKENS=bearychat-token
-export HUBOT_BEARYCHAT_MODE=http
+export HUBOT_BEARYCHAT_MODE=rtm
 export EXPRESS_PORT=7001
 export REDIS_URL=redis://:password@localhost:6379/prefix
 ./bin/hubot -a bearychat
